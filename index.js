3d7632d35f0ec544f7be58504f1e7dbc2e92c88d
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2023-09-05 11:50:25 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello world")
+
+
+/*
+.brake
+.clear
+.editor
+.exit
+.help
+.load
+.save namefile
+.rm namefile 
+.cat namefile
+
+.join
+.basename
+.resolve  
+
+
+*/
+
+const myName = "Jake"
+
+console.log(myName)
+
+console.log(__dirname)
+console.log(__filename)
+console.log(require)
+console.log(module)
+console.log(process)
+
+
